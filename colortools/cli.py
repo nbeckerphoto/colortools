1d01019f61358e8c6f2e09577b76cfcdc632746f
Nick Becker <nbeckerphoto@gmail.com>
2022-07-14 22:40:05 -0500

    Clean up CLI. Add sort by value. Refactor sort.py. Add __main__.py.

--- colortools/cli.py
+++ colortools/cli.py
@@ -4,14 +4,14 @@ import logging
 from pathlib import Path
 
 from tqdm import tqdm
-from colortools import __version__
 
 import colortools.config as config
 import colortools.util as util
 import colortools.visualization as visualization
+from colortools import __version__
 from colortools.analyzed_image import AnalyzedImage
 from colortools.heuristics import NColorsHeuristic
-from colortools.sort import colorsort
+from colortools.sort import SortMethod, colorsort, separate_color_and_bw, valuesort
 
 logging.basicConfig(format="%(levelname)s: %(message)s")
 
@@ -22,53 +22,57 @@ def parse_args() -> argparse.Namespace:
     Returns:
         argparse.Namespace: The arguments parsed from the commandline interface.
     """
-    parser = argparse.ArgumentParser(description="ColorTools")
-    parser.add_argument("input", type=Path, help="Input directory of .jpg files (or a single .jpg file).")
+    parser = argparse.ArgumentParser(description="Analyze and sort images by their dominant colors.")
+    parser.add_argument("input", type=Path, help="input directory of .jpg files (or a single .jpg file)")
     parser.add_argument("--version", action="version", version=__version__)
     parser.add_argument(
         "--algorithm",
-        "-a",
         type=util.DominantColorAlgorithm,
+        choices=[dca.value for dca in util.DominantColorAlgorithm],
         default=config.DEFAULT_DOMINANT_COLOR_ALGORITHM,
-        help="The algorithm to use for determining the dominant color of images (k-means clustering by default).",
+        help="algorithm to use for determining the dominant color of images (default kmeans)",
     )
     parser.add_argument(
         "--n_colors",
-        "-n",
         type=int,
         default=config.DEFAULT_N_COLORS,
-        help="Number of dominant colors to compute.",
+        help="number of dominant colors to compute",
     )
     parser.add_argument(
         "--n_colors_heuristic",
         type=NColorsHeuristic,
+        choices=[nch.value for nch in NColorsHeuristic],
         default=config.DEFAULT_N_COLORS_HEURISTIC,
-        help="The heuristic used to set `n` for the clustering algorithm.",
+        help="heuristic used to set `n` for the clustering algorithm",
     )
     parser.add_argument(
         "--exclude_bw",
         action="store_true",
-        help="Exclude black and white images from generated graphics.",
+        help="exclude black and white images from generated graphics",
     )
     parser.add_argument(
-        "--skip_sort", action="store_true", help="Skip sorting for the current set of analyzed images."
+        "--exclude_color",
+        action="store_true",
+        help="exclude color images from generated graphics",
     )
-    parser.add_argument("--sort_reverse", action="store_true", help="Reverse the image sort order.")
+    parser.add_argument(
+        "--sort", type=SortMethod, choices=[sm.value for sm in SortMethod], default=None, help="sort images"
+    )
+    parser.add_argument("--sort_reverse", action="store_true", help="reverse the image sort order")
     parser.add_argument(
         "--sort_anchor",
         type=str,
-        help="Anchor image with which to begin sorted sequence.",
+        help="anchor image with which to begin sorted sequence",
     )
-    parser.add_argument("--save_sorted", action="store_true", help="Save sorted sequence of images.")
+    parser.add_argument("--save_sorted", action="store_true", help="save sorted sequence of images")
     parser.add_argument(
         "--display",
         action="store_true",
-        help="Display generated graphics in addition to saving them.",
+        help="display generated graphics in addition to saving them",
     )
-    parser.add_argument("--verbose", action="store_true", help="Print a summary of the supplied arguments.")
+    parser.add_argument("--verbose", action="store_true", help="print a summary of the supplied arguments")
     parser.add_argument(
         "--output_dir",
-        "-o",
         type=Path,
         default=Path(config.DEFAULT_OUTPUT_DIR),
         help="Output directory for sorted .jpg files.",
@@ -76,27 +80,25 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--dominant_colors",
         action="store_true",
-        help="Save dominant color visualization for each image.",
+        help="save dominant color visualization for each image",
     )
     parser.add_argument(
         "--dominant_colors_remapped",
         action="store_true",
-        help="Include remapped image in dominant color visualization. Ignored if not using KMEANS algorithm.",
+        help="include remapped image in dominant color visualization; ignored if not using kmeans algorithm",
     )
     parser.add_argument(
         "--spectrum",
         action="store_true",
-        help="Save spectrum image for the current collection of images.",
+        help="save spectrum image for the current collection of images",
     )
     parser.add_argument(
         "--spectrum_all_colors",
         action="store_true",
-        help="Include all detected dominant colors in the spectrum graphic.",
+        help="include all detected dominant colors in the spectrum graphic",
     )
-    parser.add_argument("--collage", action="store_true", help="Save a collage of the analyzed images.")
-    parser.add_argument(
-        "--summary", action="store_true", help="Print a summary of the analyzed images to the console."
-    )
+    parser.add_argument("--collage", action="store_true", help="save a collage of the analyzed images")
+    parser.add_argument("--summary", action="store_true", help="print a summary of the analyzed images to the console")
     return parser.parse_args()
 
 
@@ -109,14 +111,19 @@ def check_args(args: argparse.Namespace) -> argparse.Namespace:
     Returns:
         argparse.Namespace: The checked arguments.
     """
-    if args.skip_sort and args.save_sorted:
-        logging.warning("Unable to save sorted images because skip_sort=True; ignoring save_sorted")
-        args.save_sorted = False
+    if args.save_sorted and not args.sort:
+        args.sort = config.DEFAULT_SORT_METHDO
     if not args.algorithm == util.DominantColorAlgorithm.KMEANS and args.dominant_colors_remapped:
-        logging.warning("Unable to remap image using HUE_DIST algorithm; ignoring --dominant_colors_remapped")
+        logging.warning("Unable to remap image using hue_dist algorithm; ignoring --dominant_colors_remapped")
         args.dominant_colors_remapped = False
-    if not (args.save_sorted or args.dominant_colors or args.spectrum or args.collage):
-        logging.warning("No output graphics are set to be generated! See usage (--help) for more details.")
+    if args.exclude_bw and args.exclude_color:
+        logging.error("Cannot set both --exclude_bw and --exclude_color")
+        return None
+    if not (args.sort or args.save_sorted or args.dominant_colors or args.spectrum or args.collage):
+        logging.error(
+            "No output action selected; please select --sort, --save_sorted, --dominant_colors, or --spectrum"
+        )
+        return None
     return args
 
 
@@ -136,8 +143,11 @@ def print_verbose_output(args: argparse.Namespace):
 
     print("Action summary:")
     print(f"- Black and white images will {'not ' if args.exclude_bw else ''}be included")
-    if not args.skip_sort:
-        print(f"- Images will be sorted in {'reverse' if args.sort_reverse else 'standard'} order")
+    print(f"- Color images images will {'not ' if args.exclude_color else ''}be included")
+    if args.sort:
+        print(
+            f"- Images will be sorted by {args.sort.value} in {'reverse' if args.sort_reverse else 'standard'} order"
+        )
         if args.save_sorted:
             sorted_dir = f"{args.output_dir}/{config.DEFAULT_SORTED_DIR}"
             print(f"- Sorted images will be saved to {sorted_dir}")
@@ -172,72 +182,85 @@ def print_verbose_output(args: argparse.Namespace):
     print()
 
 
-def main():
+def run():
     args = check_args(parse_args())
-    timstamp_str = util.get_timestamp_string()
-    jpg_paths = util.collect_jpg_paths(args.input)
+    if args:
+        timstamp_str = util.get_timestamp_string()
+        jpg_paths = util.collect_jpg_paths(args.input)
 
-    if args.verbose:
-        print_verbose_output()
+        if args.verbose:
+            print_verbose_output(args)
 
-    print(f"Analyzing {len(jpg_paths)} images...")
-    analyzed_images = []
-    for jpg_path in tqdm(jpg_paths, ascii=True):
-        analyzed_images.append(
-            AnalyzedImage(
-                image_path=jpg_path,
-                resize_long_axis=config.DEFAULT_RESIZE_LONG_AXIS,
-                dominant_color_algorithm=args.algorithm,
-                n_colors=args.n_colors,
-                auto_n_heuristic=args.n_colors_heuristic,
-            )
-        )
+        n_jpg_paths = len(jpg_paths)
+        if n_jpg_paths == 0:
+            print(f"No images found in {args.input}")
+        else:
+            print(f"Analyzing {n_jpg_paths} images...")
+            analyzed_images = []
+            for jpg_path in tqdm(jpg_paths, ascii=True):
+                analyzed_images.append(
+                    AnalyzedImage(
+                        image_path=jpg_path,
+                        resize_long_axis=config.DEFAULT_RESIZE_LONG_AXIS,
+                        dominant_color_algorithm=args.algorithm,
+                        n_colors=args.n_colors,
+                        auto_n_heuristic=args.n_colors_heuristic,
+                    )
+                )
 
-    if not args.skip_sort:
-        analyzed_images, color_sorted, _ = colorsort(analyzed_images, args.sort_anchor)
-        if args.exclude_bw:
-            analyzed_images = color_sorted
-        print("Sorting complete")
+            if args.exclude_bw:
+                analyzed_images, _ = separate_color_and_bw(analyzed_images)
+            if args.exclude_color:
+                _, analyzed_image = separate_color_and_bw(analyzed_images)
 
-        if args.save_sorted:
-            dest_dir = Path(f"{args.output_dir}/{config.DEFAULT_SORTED_DIR}/{timstamp_str}")
-            for i, analyzed_image in enumerate(analyzed_images):
-                sorted_image_dest = dest_dir / analyzed_image.generate_filename(i, "sorted")
-                visualization.save(analyzed_image, sorted_image_dest)
-            print(f"Saved sorted images to {dest_dir}")
+            if args.sort:
+                if args.sort == SortMethod.COLOR:
+                    analyzed_images = colorsort(analyzed_images, args.sort_anchor)
+                elif args.sort == SortMethod.VALUE:
+                    analyzed_images = valuesort(analyzed_images, args.sort_anchor)
+                else:
+                    logging.error(f"Invalid sorting method {args.sort} (how did we get here?)")
+                n_sorted = len(analyzed_images)
 
-    if args.dominant_colors or args.dominant_colors_remapped:
-        dest_dir = Path(f"{args.output_dir}/{config.DEFAULT_DOMINANT_COLOR_DIR}/{timstamp_str}")
-        for i, analyzed_image in enumerate(analyzed_images):
-            dominant_colors_dest = dest_dir / analyzed_image.generate_filename(i, "dc")
-            visualization.save_dominant_color_visualization(
-                analyzed_image,
-                config.DEFAULT_DOMINANT_COLOR_CHIP_SIZE,
-                dominant_colors_dest,
-                include_remapped_image=args.dominant_colors_remapped,
-                display=args.display,
-            )
-        print(f"Saved dominant color graphics to {dest_dir}")
+                if args.save_sorted:
+                    dest_dir = Path(f"{args.output_dir}/{config.DEFAULT_SORTED_DIR}/{timstamp_str}")
+                    for i, analyzed_image in enumerate(analyzed_images):
+                        sorted_image_dest = dest_dir / analyzed_image.generate_filename(i, "sorted")
+                        visualization.save(analyzed_image, sorted_image_dest)
+                    print(f"Saved {n_sorted} sorted images to {dest_dir}")
+                else:
+                    print(f"Sorted {n_sorted} images:")
+                    for i, image in enumerate(analyzed_images):
+                        print(f"{i+1:4.0f}. {image.image_path}")
 
-    if args.spectrum:
-        filename = f"{timstamp_str}_spectrum.jpg"
-        spectrum_dest = Path(f"{args.output_dir}/{config.DEFAULT_SPECTRUM_DIR}/{filename}")
-        visualization.save_spectrum_visualization(
-            analyzed_images, args.spectrum_all_colors, spectrum_dest, args.display
-        )
-        print(f"Saved spectrum graphic to {spectrum_dest}")
+            if args.dominant_colors or args.dominant_colors_remapped:
+                dest_dir = Path(f"{args.output_dir}/{config.DEFAULT_DOMINANT_COLOR_DIR}/{timstamp_str}")
+                for i, analyzed_image in enumerate(analyzed_images):
+                    dominant_colors_dest = dest_dir / analyzed_image.generate_filename(i, "dc")
+                    visualization.save_dominant_color_visualization(
+                        analyzed_image,
+                        config.DEFAULT_DOMINANT_COLOR_CHIP_SIZE,
+                        dominant_colors_dest,
+                        include_remapped_image=args.dominant_colors_remapped,
+                        display=args.display,
+                    )
+                print(f"Saved dominant color graphics to {dest_dir}")
 
-    if args.collage:
-        filename = f"{timstamp_str}_collage_n.jpg"
-        collage_dest = Path(f"{args.output_dir}/{config.DEFAULT_COLLAGE_DIR}/{filename}")
-        visualization.save_image_collage(analyzed_images, collage_dest, args.display)
-        print(f"Saved collage graphic to {collage_dest}")
+            if args.spectrum:
+                filename = f"{timstamp_str}_spectrum.jpg"
+                spectrum_dest = Path(f"{args.output_dir}/{config.DEFAULT_SPECTRUM_DIR}/{filename}")
+                visualization.save_spectrum_visualization(
+                    analyzed_images, args.spectrum_all_colors, spectrum_dest, args.display
+                )
+                print(f"Saved spectrum graphic to {spectrum_dest}")
 
-    if args.summary:
-        print("\nAnalyzed image summary:")
-        for i, image in enumerate(analyzed_images):
-            print(f"{i+1}. {image.get_pretty_string()}")
+            if args.collage:
+                filename = f"{timstamp_str}_collage.jpg"
+                collage_dest = Path(f"{args.output_dir}/{config.DEFAULT_COLLAGE_DIR}/{filename}")
+                visualization.save_image_collage(analyzed_images, collage_dest, args.display)
+                print(f"Saved collage graphic to {collage_dest}")
 
-
-if __name__ == "__main__":
-    main()
+            if args.summary:
+                print("\nAnalyzed image summary:")
+                for i, image in enumerate(analyzed_images):
+                    print(f"{i+1}. {image.get_pretty_string()}")
--- colortools/sort.py
+++ colortools/sort.py
@@ -1,34 +1,82 @@
 from collections import deque
+from enum import Enum
 from typing import List, Tuple
 
 from colortools.analyzed_image import AnalyzedImage
+import logging
 
 
-def colorsort(
-    image_reps: List[AnalyzedImage], anchor_image: str
-) -> Tuple[List[AnalyzedImage], List[AnalyzedImage], List[AnalyzedImage]]:
-    """Static method for sorting a collection of analyzed images by their hue.
+class SortMethod(str, Enum):
+    """Enum for image sorting methods."""
 
-    Uses the AnalyzedImage.get_sort_metric() function for sorting color images.
+    COLOR = "color"
+    VALUE = "value"
 
-    TODO: add more sorting options here
+
+def separate_color_and_bw(analyzed_images: List[AnalyzedImage]) -> Tuple[List[AnalyzedImage], List[AnalyzedImage]]:
+    """Separate a list of images into a list of color images and a list of black and white images.
+
+    Args:
+        analyzed_images (List[AnalyzedImage]): A list of analyzed images
 
     Returns:
-        Tuple[List["AnalyzedImage"], List["AnalyzedImage"], List["AnalyzedImage"]]: Three lists:
-            - The first list is all analyzed images. Color images are first, sorted by hue, followed by black
-                and white images, sorted by value.
-            - The second list is just the color images, sorted by hue.
-            - The third list is just the black and white images, sorted by value.
+        Tuple[List[AnalyzedImage], List[AnalyzedImage]]: A list of color images followed by a list
+            of black and white images.
     """
     color = []
     bw = []
-    for img in image_reps:
+    for img in analyzed_images:
         if img.is_bw():
             bw.append(img)
         else:
             color.append(img)
 
-    # sort by hue, then value
+    return color, bw
+
+
+def orient_to_sort_anchor(sorted_analyzed_images: List[AnalyzedImage], sort_anchor: str) -> List[AnalyzedImage]:
+    """Shift a sequence of sorted images so that it starts with the provided sort anchor.
+
+    Args:
+        sorted_analyzed_images (List[AnalyzedImage]): A sorted list of analyzed images.
+        sort_anchor (str): The anchor image with whih to begin the returned sorted sequence.
+
+    Returns:
+        List[AnalyzedImage]: Sorted results, starting with the sort anchor.
+    """
+    starting_index = 0
+    if sort_anchor:
+        try:
+            while not sorted_analyzed_images[starting_index].image_path.name == sort_anchor:
+                starting_index += 1
+        except IndexError:
+            logging.warning(f"Starting image {sort_anchor} not found!")
+            starting_index = 0
+
+    sorted_analyzed_images = deque(sorted_analyzed_images)
+    for _ in range(starting_index):
+        sorted_analyzed_images.append(sorted_analyzed_images.popleft())
+
+    return list(sorted_analyzed_images)
+
+
+def colorsort(analyzed_images: List[AnalyzedImage], sort_anchor: str) -> List[AnalyzedImage]:
+    """Static method for sorting a collection of analyzed images by their hue.
+
+    Uses the AnalyzedImage.get_sort_metric() function for sorting color images.
+
+    Args:
+        analyzed_images (List[AnalyzedImage]): A list of analyzed images.
+        sort_anchor (str): The anchor image with whih to begin the returned sorted sequence.
+
+    Returns:
+        List[AnalyzedImage]: Sorted results, where color images are first, sorted by the hue of
+            their dominant color, followed by black and white images, sorted by the value of
+            their dominant color.
+    """
+    color, bw = separate_color_and_bw(analyzed_images)
+
+    # sort color images by built-in sort metric, then value
     color.sort(
         key=lambda elem: (
             elem.get_sort_metric(),
@@ -36,23 +84,28 @@ def colorsort(
         )
     )
 
-    # sort by value
+    # sort black and white images by value
     bw.sort(key=lambda elem: elem.get_dominant_color(hsv=True)[2])
 
-    starting_index = 0
-    if anchor_image:
-        try:
-            while not color[starting_index].image_path.name == anchor_image:
-                starting_index += 1
-        except IndexError:
-            print(f"Starting image {anchor_image} not found!")
-            starting_index = 0
-
-    color = deque(color)
-    for _ in range(starting_index):
-        color.append(color.popleft())
-
+    color = orient_to_sort_anchor(color, sort_anchor)
     combined = []
     combined.extend(color)
     combined.extend(bw)
-    return combined, color, bw
+    return combined
+
+
+def valuesort(analyzed_images: List[AnalyzedImage], sort_anchor: str) -> List[AnalyzedImage]:
+    """Static method for sorting a collection of analyzed images by their value.
+
+    Args:
+        analyzed_images (List[AnalyzedImage]): A list of analyzed images.
+        sort_anchor (str): The anchor image with whih to begin the returned sorted sequence.
+
+    Returns:
+        List[AnalyzedImage]: Sorted results, where all images are sorted by the value of
+            their domiant color.
+    """
+
+    # sort color images by built-in sort metric, then value
+    analyzed_images.sort(key=lambda elem: (elem.get_dominant_color(hsv=True)[2], elem.get_sort_metric()))
+    return orient_to_sort_anchor(orient_to_sort_anchor(analyzed_images, sort_anchor))
